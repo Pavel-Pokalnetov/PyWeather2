9b5aa7dae8d55f7618e2c585e32d69fe5c49035a
Pokalnetov Pavel <pokalnetovps@gmail.com>
2023-06-23 11:10:46 +0500

    + сохранение полученных данных в weather.html

--- run.cmd
+++ run.cmd
@@ -2,6 +2,8 @@
 rem Активируем виртуальное окружение
 call venv\Scripts\activate.bat
 rem Запускаем приложение
-python app.py
+python app.py > weather.html
 rem Деактивируем виртуальное окружение
 call venv\Scripts\deactivate.bat
+
+
